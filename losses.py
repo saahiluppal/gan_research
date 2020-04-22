5a79e4cc2fb68dabc5270b9d56c14e74e01aa736
Sahil Uppal <sahiluppal2k@gmail.com>
2020-04-22 11:11:12 +0530

    added modified loss

--- losses.py
+++ losses.py
@@ -5,8 +5,11 @@ _all_losses = [
     'minimax_generator_loss',
     'tf_minimax_discriminator_loss',
     'tf_minimax_generator_loss',
+    'modified_discriminator_loss',
+    'modified_generator_loss',
 ]
 
+# Minimax Custom Implementation ->>
 def minimax_discriminator_loss(real_outputs,
                                generated_outputs,
                                real_weights=1.,
@@ -51,8 +54,7 @@ def minimax_discriminator_loss(real_outputs,
     )
 
     loss = loss_on_real + loss_on_generated
-
-    return loss
+    return tf.reduce_mean(loss)
 
 
 def minimax_generator_loss(generated_outputs,
@@ -78,15 +80,15 @@ def minimax_generator_loss(generated_outputs,
         smoothing
     )
 
-    return loss
+    return tf.reduce_mean(loss)
 
-
+# Minimax Tensorflow's Implementation ->>
 def tf_minimax_discriminator_loss(real_outputs,
                                   generated_outputs,
                                   smoothing=0.25,
                                   real_weights=1.0,
                                   generated_weights=1.0,
-            reduction=tf.compat.v1.losses.Reduction.SUM_BY_NONZERO_WEIGHTS):
+                                  reduction=tf.compat.v1.losses.Reduction.SUM_BY_NONZERO_WEIGHTS):
     """
     Args:
     real_outputs: Discriminator output on real data.
@@ -128,7 +130,7 @@ def tf_minimax_discriminator_loss(real_outputs,
 def tf_minimax_generator_loss(generated_outputs,
                               smoothing=0.,
                               weights=1.,
-            reduction=tf.compat.v1.losses.Reduction.SUM_BY_NONZERO_WEIGHTS):
+                              reduction=tf.compat.v1.losses.Reduction.SUM_BY_NONZERO_WEIGHTS):
     """
     Args:
     generated_outputs: Discriminator output on generated data. Expected 
@@ -153,12 +155,143 @@ def tf_minimax_generator_loss(generated_outputs,
 
     return loss
 
+# Modified Custom Implementation ->>
+def modified_discriminator_loss(real_outputs,
+                                generated_outputs,
+                                real_weights=1.0,
+                                generated_weights=1.0,
+                                smoothing=0.25):
+    """
+    Same as minimax discriminator loss.
 
+    Args:
+    real_outputs: Discriminator output on real data.
+    generated_outputs: Discriminator output on generated data. Expected
+        to be in range of (-inf, inf)
+    real_weights: Optional `Tensor` whose rank is either 0, or the same rank as
+      `discriminator_gen_outputs`, and must be broadcastable to
+      `discriminator_gen_outputs` (i.e., all dimensions must be either `1`, or
+      the same as the corresponding dimension).
+    generated_weights: Same as `real_weights`, but for
+      `discriminator_gen_outputs`.
+    smoothing: The amount of smoothing for positive labels. This technique
+      is taken from `Improved Techniques for Training GANs`
+      (https://arxiv.org/abs/1606.03498). `0.0` means no smoothing.
+    """
+    loss = minimax_discriminator_loss(
+        real_outputs,
+        generated_outputs,
+        real_weights,
+        generated_weights,
+        smoothing
+    )
+
+    return tf.reduce_mean(loss)
+
+def modified_generator_loss(generated_outputs,
+                            smoothing=0.0,
+                            weights=1.0):
+    """
+    Args:
+    generated_outputs: Discriminator output on generated data. Expected
+      to be in the range of (-inf, inf).
+    smoothing: The amount of smoothing for positive labels. This technique
+      is taken from `Improved Techniques for Training GANs`
+      (https://arxiv.org/abs/1606.03498). `0.0` means no smoothing.
+    weights: Optional `Tensor` whose rank is either 0, or the same rank as
+      `discriminator_gen_outputs`, and must be broadcastable to `labels` (i.e.,
+      all dimensions must be either `1`, or the same as the corresponding
+      dimension).
+    
+    L = - log(sigmoid(D(G(z))))
+    """
+    if smoothing > 0:
+        generated_outputs = (generated_outputs * (1 - smoothing) +
+                        0.5 * smoothing)
+
+    loss = tf.nn.weighted_cross_entropy_with_logits(
+        labels = tf.ones_like(generated_outputs),
+        logits = generated_outputs,
+        pos_weight = weights,
+    )
+
+    return tf.reduce_mean(loss)
+
+def tf_modified_discriminator_loss(real_outputs,
+                                  generated_outputs,
+                                  smoothing=0.25,
+                                  real_weights=1.0,
+                                  generated_weights=1.0,
+                                  reduction=tf.compat.v1.losses.Reduction.SUM_BY_NONZERO_WEIGHTS):
+    """
+    Same as tf minimax discriminator loss
+    Args:
+    real_outputs: Discriminator output on real data.
+    generated_outputs: Discriminator output on generated data. Expected
+      to be in the range of (-inf, inf).
+    smoothing: The amount of smoothing for positive labels. This technique
+      is taken from `Improved Techniques for Training GANs`
+      (https://arxiv.org/abs/1606.03498). `0.0` means no smoothing.
+    real_weights: Optional `Tensor` whose rank is either 0, or the same rank as
+      `real_data`, and must be broadcastable to `real_data` (i.e., all
+      dimensions must be either `1`, or the same as the corresponding
+      dimension).
+    generated_weights: Same as `real_weights`, but for `generated_data`.
+    reduction: A `tf.losses.Reduction` to apply to loss.
+    """
+    return tf_minimax_discriminator_loss(
+        real_outputs,
+        generated_outputs,
+        smoothing,
+        real_weights,
+        generated_weights,
+        reduction,
+    )
+
+def tf_modified_generator_loss(generated_outputs,
+                                smoothing = 0.0,
+                                weights = 1.0,
+                                reduction = tf.compat.v1.losses.Reduction.SUM_BY_NONZERO_WEIGHTS):
+    """
+    Args:
+    generated_outputs: Discriminator output on generated data. Expected
+      to be in the range of (-inf, inf).
+    smoothing: The amount of smoothing for positive labels. This technique
+      is taken from `Improved Techniques for Training GANs`
+      (https://arxiv.org/abs/1606.03498). `0.0` means no smoothing.
+    weights: Optional `Tensor` whose rank is either 0, or the same rank as
+      `discriminator_gen_outputs`, and must be broadcastable to `labels` (i.e.,
+      all dimensions must be either `1`, or the same as the corresponding
+      dimension).
+    reduction: A `tf.losses.Reduction` to apply to loss.
+
+    L = -log(sigmoid(D(G(z))))
+    """
+    loss = tf.compat.v1.losses.sigmoid_cross_entropy(
+        multi_class_labels = tf.ones_like(generated_outputs),
+        logits = generated_outputs,
+        weights = weights,
+        label_smoothing = smoothing,
+        reduction = reduction,
+    )
+    return loss
+
+## Testing Implementations:
 real_output = tf.constant([-5.0, 1.4, 12.5, 2.7])
 generated_output = tf.constant([10.0, 4.4, -5.5, 3.6])
 
-print(tf_minimax_discriminator_loss(real_output, generated_output))
-print(tf_minimax_generator_loss(generated_output))
+minimax_expected_d_loss = 6.19637
+minimiax_expected_g_loss = -4.82408
 
-expected_d_loss = 6.19637
-expected_g_loss = -4.82408
+modified_expected_d_loss = 6.19637
+modified_expected_g_loss = 1.38582
+
+
+print(minimax_discriminator_loss(real_output, generated_output))
+print(minimax_generator_loss(generated_output))
+
+print(modified_discriminator_loss(real_output, generated_output))
+print(modified_generator_loss(generated_output))
+
+print(tf_modified_discriminator_loss(real_output, generated_output))
+print(tf_modified_generator_loss(generated_output))
